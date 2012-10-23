3a84ccd62905f5daa755bf60b83abd17a9cdcb20
Ilya Sadykov <smecsia@gmail.com>
2012-10-23 15:48:10 +0400

    extension updated

--- src/main/java/me/smecsia/smartfox/example/ExampleExtension.java
+++ src/main/java/me/smecsia/smartfox/example/ExampleExtension.java
@@ -1,11 +1,21 @@
 package me.smecsia.smartfox.example;
 
+import com.smartfoxserver.v2.core.SFSEventType;
+import com.smartfoxserver.v2.extensions.SFSExtension;
+import me.smecsia.smartfox.example.common.Constants;
+import me.smecsia.smartfox.example.handlers.OnUserLoginHandler;
+import me.smecsia.smartfox.example.handlers.PrivateMessageHandler;
+
 /**
- * Copyright (c) 2012 i-Free. All Rights Reserved.
- *
  * @author Ilya Sadykov
  *         Date: 23.10.12
  *         Time: 15:28
  */
-public class ExampleExtension {
+public class ExampleExtension extends SFSExtension {
+    @Override
+    public void init() {
+        addRequestHandler(Constants.Request.PRIVATE_MESSAGE, PrivateMessageHandler.class);
+
+        addEventHandler(SFSEventType.USER_LOGIN, OnUserLoginHandler.class);
+    }
 }
